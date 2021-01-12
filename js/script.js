416080e53831e9daa35e912d21c88e8885b046a5
Enrique Ferrer <emferrer4@gmail.com>
2021-01-12 11:18:40 +0800

    preparing to duplicate repo

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const searchForm = document.querySelector(".search");
+
+searchForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e){
+    e.preventDefault();
+    console.log("Clicked!");
+    searchForm.reset();
+}
+
+const endpoint = 'https://forkify-api.herokuapp.com/api/search?q=';
+const input = 'pizza'
+let list = [];
+async function getRecipe() {
+    list = [];
+    console.log(endpoint+input)
+	try {
+        const response = await fetch(endpoint+input)
+		if (!response.ok) {
+			throw Error(response.statusText)
+		}
+        const json = await response.json();
+        console.log(json);
+        list = [json.recipes];
+
+        console.log(list)
+
+        list.forEach(recipeList => {
+            recipeList.forEach(recipe => console.log(recipe))
+        });
+
+        list = []
+        console.log(list)
+        
+        // json.recipes.forEach(recipe => {
+        //     console.log(recipe)
+        // });
+	} catch (err) {
+		console.log(err)
+		alert('Failed to get recipe');
+	}
+}
+
+getRecipe();
